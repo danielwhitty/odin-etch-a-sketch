aeb333d8bc7baab34ac7f5293e8022a4b2b7a115
Daniel Whitty <d.james.whitty@gmail.com>
2022-09-23 21:54:10 +1000

    Increment black opacity by 10%

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ function createDivs(num) {
             
             // Add colour to div when mousing over
             sketchDiv.addEventListener('mouseover', function () {
-                sketchDiv.classList.add('hover-colour');
+                incrementOpacity(sketchDiv);
             });
 
             container.appendChild(sketchDiv);
@@ -38,3 +38,40 @@ btnSize.addEventListener('click', function() {
     removeDivs();
     createDivs(gridSize);
 });
+
+function incrementOpacity(div) {
+    const currentColour = div.style.background;
+    switch (currentColour) {
+        case ('rgba(0, 0, 0, 0.1)'):
+            div.style.background = 'rgba(0, 0, 0, 0.2)';
+            break;
+        case ('rgba(0, 0, 0, 0.2)'):
+            div.style.background = 'rgba(0, 0, 0, 0.3)';
+            break;
+        case ('rgba(0, 0, 0, 0.3)'):
+            div.style.background = 'rgba(0, 0, 0, 0.4)';
+            break;
+        case ('rgba(0, 0, 0, 0.4)'):
+            div.style.background = 'rgba(0, 0, 0, 0.5)';
+            break;
+        case ('rgba(0, 0, 0, 0.5)'):                     
+            div.style.background = 'rgba(0, 0, 0, 0.6)';
+            break;
+        case ('rgba(0, 0, 0, 0.6)'):
+            div.style.background = 'rgba(0, 0, 0, 0.7)';
+            break;
+        case ('rgba(0, 0, 0, 0.7)'):
+            div.style.background = 'rgba(0, 0, 0, 0.8)';
+            break;
+        case ('rgba(0, 0, 0, 0.8)'):
+            div.style.background = 'rgba(0, 0, 0, 0.9)';
+            break;
+        case ('rgba(0, 0, 0, 0.9)'):
+        case ('rgb(0, 0, 0)'):
+            div.style.background = 'rgba(0, 0, 0, 1)';
+            break;
+        default:
+            div.style.background = 'rgba(0, 0, 0, 0.1)';
+            break;
+    }
+}
